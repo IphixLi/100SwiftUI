a6f4f517c1fca9453793408c64816e2795e12444
IphixLi <iphigeniebera2024@u.northwestern.edu>
2024-02-27 16:59:32 -0600

    day 31

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,45 +8,168 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"];
-
-    var body: some View {
-        List{
-            Text("Static Row")
-            ForEach(people, id: \.self) {
-                Text($0)
-            }
-        }
-    }
-        
-    func testBundles(){
-            if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-                if let fileContents = try? String(contentsOf: fileURL) {
-                        // we loaded the file into a string!
-                    }
-            }
-                
-        }
+    @State private var usedWords=[String]()
+    @State private var rootWord=""
+    @State private var newWord=""
     
-    func testStrings(){
-        let input="a b c"
-        let letters=input.components(separatedBy: " ")
-        let letter = letters.randomElement()
-        let word = "swift"
+    @State private var errorTitle=""
+    @State private var errorMessage=""
+    @State private var showingError=false
+    
+    
+    // challenge3
+    @State private var letter_count=0
+    @State private var word_count=0
+    
+    func wordError(title:String, message:String){
+        errorTitle=title
+        errorMessage=message
+        showingError=true
+    }
+    
+    var body: some View {
+        NavigationStack{
+            List{
+                
+                Section("Score"){
+                    HStack{
+                        Text("word count:")
+                        Text("\(word_count)")
+                    }
+                    HStack{
+                        Text("letter count:")
+                        Text("\(letter_count)")
+                    }
+                }
+                
+                
+                Section{
+                    TextField("Enter your word", text:$newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section{
+                    ForEach(usedWords, id:\.self){word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)}
+                    }
+                }
+                
+            }.navigationTitle(rootWord)
+            .onSubmit {
+                        addNewWord()
+                    }
+            .onAppear(perform:startGame)
+            .alert(errorTitle, isPresented:$showingError){
+                Button("OK"){}
+            }message:{
+                Text(errorMessage)
+            }
+            .toolbar{
+                ToolbarItem(placement: .bottomBar){
+                    Button("Restart Game"){
+                        print("hahahaha")
+                        startGame()
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func addNewWord(){
+        let answer=newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        // trimming
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count>0 else{
+            print(answer)
+            return }
         
-        //spelling check
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
         
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
-        let allGood = misspelledRange.location == NSNotFound
+        guard answer.utf8.count >= 3 else{
+            wordError(title: "Short word", message: "Word should be atleast 3 characters")
+            return
+        }
 
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        
+        withAnimation{
+            usedWords.insert(answer, at:0)
+            newWord=""
+            word_count+=1
+            letter_count+=answer.utf16.count
+        }
     }
+    
+    func startGame(){
+        
+        if let startWordsURL=Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords=try? String(contentsOf:startWordsURL){
+                
+                let allWords=startWords.components(separatedBy: "\n")
+                
+                rootWord=allWords.randomElement() ?? "silkworm"
+                
+                word_count=0
+                letter_count=0
+                print(rootWord)
+                return
+            }
+            
+        }
+        fatalError("Could not load start.txt from bundle.")
     }
+    
+    func lessthanthree(word:String)->Bool{
+        if word.utf16.count<=3{
+            return true
+        }
+        return false
+    }
+    
+    func isOriginal(word: String)->Bool{
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String)->Bool{
+        var tempWord=rootWord
+        
+        for letter in word{
+            if let pos = tempWord.firstIndex(of:letter){
+                tempWord.remove(at:pos)
+            }else{
+                return false
+            }
+        }
+        
+        return true
+}
+    
+    func isReal(word:String)->Bool{
+        let checker=UITextChecker()
+        let range=NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange=checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location==NSNotFound
+    }
+    
+    
+    
+    
+}
 
 
 #Preview {
